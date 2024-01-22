158b914f89aa90c06374db4165b52b891fe2a883
Daniel Jones <145261080+DanielJones02@users.noreply.github.com>
2024-01-22 17:44:57 +0000

    Update main.py
    changed presence to listening and commented some code also removed some stuff from todo

--- src/main.py
+++ src/main.py
@@ -13,9 +13,7 @@ bot = commands.Bot(command_prefix="/", intents=intents)
 # MY STUPID FUCKING TODO LIST:
     
 # ADD COMMAND COOLDOWNS
-# IMPLEMENT ECONOMY GAME
 # ADD DASHBOARD (TK OR GRADIO/HTML,CSS,JS)
-# ADD ROLE ADDING FUNCTION (buy roles from shop)
 
 
 @bot.event
@@ -23,9 +21,10 @@ async def on_ready():
     await lock_function(bot, save_locked_channels, unlock_channel_after_delay)
 
     print(f"{t}{Fore.LIGHTBLUE_EX} | Ready and online - {bot.user.display_name}\n{Fore.RESET}")
-    await bot.change_presence(activity=discord.Game('discord | $help'))
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="/help")) # Lisening to /help (status)
+    #await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="/help")) # Wating /help (status) 
 
-    guilds(bot)
+    guilds(bot) # output guilds (if enabled)
 
 
 @bot.event
@@ -33,6 +32,7 @@ async def on_member_join(member):
     user_join(bot, member_join, welcome_channel_id, welcome_message)
 
 
+# Add cogs 
 bot.load_extension('help')
 bot.load_extension('moderation')
 bot.load_extension('fun')
