e20907b577b72691daff344c4992c3d32c777c7d
Daniel Jones <145261080+DanielJones02@users.noreply.github.com>
2024-02-01 20:44:08 +0000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,13 +1,27 @@
 
 
+from installation import SETUP_INSTALL
+SETUP_INSTALL() # installs packages. this is enabled by default. after running this for the first time, you can set 'skip_installation' to 'true'
+
+
 from utilities import *
 
 
-intents = discord.Intents.default()
-intents.members = True
-intents.messages = True 
+from crafting import crafting_setup
+from economy import economy_setup
+from farming import farming_setup
+from fun import fun_setup
+from help import help_setup
+from slots import slots_setup
+from moderation import mod_setup
+from blackjack import blackjack_setup
 
-bot = commands.Bot(command_prefix="/", intents=intents) 
+
+intents = discord.Intents.all()
+
+# Create Bot instance with intents
+bot = commands.Bot(command_prefix=config.get('prefix'), intents=intents, help_command=None)
+
 
 @bot.event
 async def on_ready():
@@ -17,19 +31,16 @@ async def on_ready():
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="/help"))
 
     guilds(bot)
+    
 
-
-@bot.event
-async def on_member_join(member):
-    user_join(bot, member_join, welcome_channel_id, welcome_message)
-
-
-# Add cogs 
-bot.load_extension('help')
-bot.load_extension('moderation')
-bot.load_extension('fun')
-bot.load_extension('economy')
-bot.load_extension('crafting')
-bot.load_extension('farming')
+# Add cogs
+crafting_setup(bot)
+economy_setup(bot)
+fun_setup(bot)
+farming_setup(bot)
+help_setup(bot)
+mod_setup(bot)
+blackjack_setup(bot)
+slots_setup(bot)
 
 bot.run(token)
