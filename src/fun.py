8ce71d16f0d0f5d3a145ac249662aca334adf06a
Daniel Jones <145261080+DanielJones02@users.noreply.github.com>
2024-04-21 15:13:43 +0100

    Update fun.py
    added author message on embeds

--- src/fun.py
+++ src/fun.py
@@ -13,28 +13,52 @@ class Fun(commands.Cog):
     async def ping(self, ctx):
         latency = round(self.bot.latency * 1000)  # Latency in milliseconds
         embed = discord.Embed(title="Ping", description=f"Pong! Latency is {latency}ms", color=embed_colour)
+
+        embed.set_footer(text=f"Made by mal023")
+
         await ctx.send(embed=embed) 
 
 
     @commands.command()
-    async def say(self, ctx, *, message: str=None):
+    async def say(self, ctx, title: str=None, *, message: str=None):
         if message is None:
             embed = discord.Embed(
-                title="Incorrect say usage",
+                title="Incorrect say command usage",
                 description=f"{ctx.author.mention}, Please specify a message. Usage: `{prefix}say <message>`",
                 color=embed_error
             )
+
+            embed.set_footer(text=f"Made by mal023")
+
             await ctx.send(embed=embed)
             return
 
-        embed = discord.Embed(title="Say", description=message, color=embed_colour) # 'description=message' set description to whatever their parsed in
+        if title is None:
+            embed = discord.Embed(
+                title="Incorrect say command usage",
+                description=f"{ctx.author.mention}, Please specify a title. Usage: `{prefix}say <title> <message>`",
+                color=embed_error
+            )
+
+            embed.set_footer(text=f"Made by mal023")
+
+            await ctx.send(embed=embed)
+            return
+
+        embed = discord.Embed(title=title, description=message, color=embed_colour) 
+
+        embed.set_footer(text=f"Made by mal023")
+
         await ctx.send(embed=embed)
 
 
     # Invite Command
     @commands.command()
     async def invite(self, ctx):
-        embed = discord.Embed(title="Invite", description=bot_invite, color=embed_colour)
+        embed = discord.Embed(title="Add me to your server!", description=bot_invite, color=embed_colour)
+
+        embed.set_footer(text=f"Made by mal023")
+
         await ctx.send(embed=embed) 
 
 
@@ -42,25 +66,35 @@ class Fun(commands.Cog):
     async def server_info(self, ctx):
         guild = ctx.guild
         embed = discord.Embed(title="Server Information", color=embed_colour)
+
         embed.set_thumbnail(url=guild.icon.url if guild.icon else None)
+
         embed.add_field(name="Server Name", value=guild.name, inline=True)
         embed.add_field(name="Server ID", value=guild.id, inline=True)
         embed.add_field(name="Members", value=guild.member_count, inline=True)
         embed.add_field(name="Owner", value=guild.owner.display_name, inline=True)
-        # Removed the line for region since it's no longer available
         embed.add_field(name="Creation Time", value=guild.created_at.strftime("%Y-%m-%d %H:%M:%S"), inline=True)
+
+        embed.set_footer(text=f"Made by mal023")
+
         await ctx.send(embed=embed)
 
 
     @commands.command(aliases=['user'])
     async def user_info(self, ctx, user: commands.MemberConverter = None):
         user = user or ctx.author  # If user is None, use the author of the command
+
         embed = discord.Embed(title="User Information", color=embed_colour)
+
         embed.set_thumbnail(url=user.avatar.url)
+
         embed.add_field(name="Username", value=user.display_name, inline=True)
         embed.add_field(name="User ID", value=user.id, inline=True)
         embed.add_field(name="Joined Server", value=user.joined_at.strftime("%Y-%m-%d %H:%M:%S"), inline=True)
         embed.add_field(name="Account Created", value=user.created_at.strftime("%Y-%m-%d %H:%M:%S"), inline=True)
+
+        embed.set_footer(text=f"Made by mal023")
+
         await ctx.send(embed=embed)
 
 
@@ -77,6 +111,8 @@ class Fun(commands.Cog):
             # User doesn't have an avatar, provide a default image or message
             embed.description = f"{ctx.author.mention}, This user does not have an avatar."
 
+        embed.set_footer(text=f"Made by mal023")
+
         await ctx.send(embed=embed)
 
 
@@ -88,12 +124,18 @@ class Fun(commands.Cog):
                 description=f"{ctx.author.mention}, Please specify a question. Usage: `{prefix}eight_ball <question>`",
                 color=embed_error
             )
+            
+            embed.set_footer(text=f"Made by mal023")
+            
             await ctx.send(embed=embed)
             return
         
         responses = ["It is certain.", "Without a doubt.", "Yes, definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."]
         answer = random.choice(responses)
         embed = discord.Embed(title=f"{ctx.author.display_name}'s, 🎱 Magic 8-Ball", description=f"Question: {question}\nAnswer: {answer}", color=embed_error)
+        
+        embed.set_footer(text=f"Made by mal023")
+        
         await ctx.send(embed=embed)
     
 
@@ -105,6 +147,9 @@ class Fun(commands.Cog):
                 description=f"{ctx.author.mention}, Please specify a link. Usage: `{prefix}qr <link>`",
                 color=embed_error
             )
+            
+            embed.set_footer(text=f"Made by mal023")
+            
             await ctx.send(embed=embed)
             return
 
@@ -138,6 +183,8 @@ class Fun(commands.Cog):
         embed = discord.Embed(title="Member Count", color=embed_colour)
         embed.add_field(name="Server Members", value=f"{ctx.author.mention}, This server has {member_count} members.", inline=False)
 
+        embed.set_footer(text=f"Made by mal023")
+        
         await ctx.send(embed=embed)
 
 
@@ -149,6 +196,8 @@ class Fun(commands.Cog):
         embed = discord.Embed(title="Dice Roll", color=embed_colour)
         embed.add_field(name="Result", value=f"{ctx.author.mention}, You rolled a {result}!", inline=False)
 
+        embed.set_footer(text=f"Made by mal023")
+        
         await ctx.send(embed=embed)
 
 
@@ -169,6 +218,8 @@ class Fun(commands.Cog):
             embed.add_field(name="Content", value=content, inline=False)
             embed.add_field(name="Author", value=author, inline=False)
 
+            embed.set_footer(text=f"Made by mal023")
+            
             await ctx.send(embed=embed)
         else: # if this happens open an error on github (this means the api is no longer valid or its offline)
             await ctx.send("Failed to fetch the daily quote. Try again later.")
@@ -179,6 +230,9 @@ class Fun(commands.Cog):
         if expression is None:
             # Handle the case where no expression is provided
             embed = discord.Embed(title="Error", description=f"{ctx.author.mention}, You need to provide a mathematical expression.", color=embed_error)
+            
+            embed.set_footer(text=f"Made by mal023")
+            
             await ctx.send(embed=embed)
             return
 
@@ -190,11 +244,15 @@ class Fun(commands.Cog):
             embed.add_field(name="Expression", value=expression, inline=False)
             embed.add_field(name="Result", value=result, inline=False)
 
+            embed.set_footer(text=f"Made by mal023")
+            
             await ctx.send(embed=embed)
         except Exception as e:
             # Create an error embed
             embed = discord.Embed(title="Error", description=str(e), color=embed_error)
 
+            embed.set_footer(text=f"Made by mal023")
+            
             await ctx.send(embed=embed)
 
 
@@ -211,6 +269,7 @@ class Fun(commands.Cog):
             embed.add_field(name="Setup", value=setup, inline=False)
             embed.add_field(name="Punchline", value=punchline, inline=False)
 
+            embed.set_footer(text=f"Made by mal023")
             # Send the embed
             await ctx.send(embed=embed)
         except Exception as e: # if this happens its probably because the api is no longer accessible (report this on github)
@@ -228,6 +287,9 @@ class Fun(commands.Cog):
                 description=f"{ctx.author.mention}, Please specify your choice: `{prefix}coinflip <heads/tails>`",
                 color=embed_error
             )
+            
+            embed.set_footer(text=f"Made by mal023")
+            
             await ctx.send(embed=embed)
             return
 
@@ -244,6 +306,8 @@ class Fun(commands.Cog):
         else:
             embed.add_field(name="Outcome", value="Better luck next time!", inline=False)
 
+        embed.set_footer(text=f"Made by mal023")
+
         await ctx.send(embed=embed)
 
 
@@ -255,6 +319,9 @@ class Fun(commands.Cog):
                 description=f"{ctx.author.mention}, Failed to add that emoji! please provide a name!",
                 color=embed_error
             )
+            
+            embed.set_footer(text=f"Made by mal023")
+            
             await ctx.send(embed=embed)
             return
         elif url is None:
@@ -263,6 +330,9 @@ class Fun(commands.Cog):
                 description=f"{ctx.author.mention}, Failed to add that emoji! Please provide a url of the emoji: e.g `https://example.com/emoji.png`",
                 color=embed_error
             )
+            
+            embed.set_footer(text=f"Made by mal023")
+            
             await ctx.send(embed=embed)
             return
 
@@ -273,6 +343,9 @@ class Fun(commands.Cog):
             description=f"{ctx.author.mention}, Emoji {name} added successfully!",
             color=embed_colour
         )
+        
+        embed.set_footer(text=f"Made by mal023")
+        
         await ctx.send(embed=embed)
 
 
@@ -284,6 +357,9 @@ class Fun(commands.Cog):
                 description=f"{ctx.author.mention}, Failed to delete that emoji! Please provide the actual emoji! e.g 💀",
                 color=embed_error
             )
+            
+            embed.set_footer(text=f"Made by mal023")
+            
             await ctx.send(embed=embed)
             return
 
@@ -295,6 +371,8 @@ class Fun(commands.Cog):
             color=embed_colour
         )
 
+        embed.set_footer(text=f"Made by mal023")
+        
         await ctx.send(embed=embed)
 
     @commands.Cog.listener()
