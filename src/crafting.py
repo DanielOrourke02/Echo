9f1c15929d7664ec99bd9ee5ab305a4fc4285e1f
Daniel Jones <145261080+DanielJones02@users.noreply.github.com>
2024-04-26 16:28:36 +0100

    Update crafting.py
    Fixed minor bugs

--- src/crafting.py
+++ src/crafting.py
@@ -45,55 +45,57 @@ class Crafting(commands.Cog):
         """
         user_id = ctx.author.id
 
-        # Check if item_name is empty
-        if item_name is None:
-            embed = discord.Embed(title="Incorrect Usage", description=f"Correct usage: `{ctx.prefix}craft <item>`", color=embed_error)
-
-            embed.set_footer(text=f"Made by mal023")
-
-            await ctx.send(embed=embed)
-            return
-
-        item_name = item_name.lower()
-
-        if item_name in crafting_recipes:
-            recipe = crafting_recipes[item_name]
-            inventory = get_user_inventory(user_id)
-            missing_items = {}
-
-            # Check for each item in the recipe
-            for ingredient, count in recipe.items():
-                if ingredient != 'result' and (inventory.count(ingredient) < count):
-                    missing_items[ingredient] = count - inventory.count(ingredient)
-
-            # If missing items
-            if missing_items:
-                missing_items_text = ', '.join([f"{count}x {item}" for item, count in missing_items.items()])
-
-                embed = discord.Embed(title="Missing Items", description=f"You are missing {missing_items_text} for crafting {item_name}.", color=embed_error)
+        try:
+            # Check if item_name is empty
+            if item_name is None:
+                embed = discord.Embed(title="Incorrect Usage", description=f"Correct usage: `{ctx.prefix}craft <item>`", color=embed_error)
 
                 embed.set_footer(text=f"Made by mal023")
 
                 await ctx.send(embed=embed)
-            else:
-                # Remove used items from inventory and add crafted item
+                return
+
+            item_name = item_name.lower()
+
+            if item_name in crafting_recipes:
+                recipe = crafting_recipes[item_name]
+                inventory = get_user_inventory(user_id)
+                missing_items = {}
+
+                # Check for each item in the recipe
                 for ingredient, count in recipe.items():
-                    if ingredient != 'result':
-                        for _ in range(count):
-                            remove_item_from_inventory(user_id, ingredient)
+                    if ingredient != 'result' and (inventory.count(ingredient) < count):
+                        missing_items[ingredient] = count - inventory.count(ingredient)
 
-                add_item_to_inventory(user_id, recipe['result'])
+                # If missing items
+                if missing_items:
+                    missing_items_text = ', '.join([f"{count}x {item}" for item, count in missing_items.items()])
 
-                embed = discord.Embed(title="Crafting Successful", description=f"You have crafted {recipe['result']}.", color=discord.Colour.green())
+                    embed = discord.Embed(title="Missing Items", description=f"You are missing {missing_items_text} for crafting {item_name}.", color=embed_error)
 
+                    embed.set_footer(text=f"Made by mal023")
+
+                    await ctx.send(embed=embed)
+                else:
+                    # Remove used items from inventory and add crafted item
+                    for ingredient, count in recipe.items():
+                        if ingredient != 'result':
+                            for _ in range(count):
+                                remove_item_from_inventory(user_id, ingredient)
+
+                    add_item_to_inventory(user_id, recipe['result'])
+
+                    embed = discord.Embed(title="Crafting Successful", description=f"You have crafted {recipe['result']}.", color=discord.Color.green())
+
+                    embed.set_footer(text=f"Made by mal023")
+
+                    await ctx.send(embed=embed)
+            else:
+                embed = discord.Embed(title="Error", description="This item cannot be crafted or does not exist.", color=embed_error)
                 embed.set_footer(text=f"Made by mal023")
-
                 await ctx.send(embed=embed)
-        else:
-            embed = discord.Embed(title="Error", description="This item cannot be crafted or does not exist.", color=embed_error)
-            embed.set_footer(text=f"Made by mal023")
-            await ctx.send(embed=embed)
-
+        except Exception as e:
+            print(e)
 
     @commands.Cog.listener()
     async def on_ready(self):
